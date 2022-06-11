d4b90971bb2e165c415637b083211b117db9357d
Rishaab Dhar <rishaabdhar@gmail.com>
2022-06-11 23:35:22 +0530

    Add API Project and Rename Controller

---
+++ CleanArch/CleanArch.API/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        
+    }
+}
